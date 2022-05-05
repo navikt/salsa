bb4091ea3a04da9be7fd127d094600f72b7b6c61
ybelMekk <youssef.bel.mekki@nav.no>
2022-05-06 00:41:40 +0200

    feat: add some deploy config and deploy to cluster

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,19 +1,28 @@
 package com.example.plugins
 
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
 
 fun Application.configureRouting() {
 
     // Starting point for a Ktor app:
     routing {
-        get("/") {
+        get("/hello") {
             call.respondText("Hello World!")
         }
     }
     routing {
+        get("/internal/isalive") {
+            call.respondText("alive!")
+        }
+    }
+
+    routing {
+        get("/internal/isready") {
+            call.respondText("ready!")
+        }
     }
 }
